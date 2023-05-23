cfb4571d2fcc9c0398e6291d5e7b9ef54c89899d
jeenunim <jeenunim@DESKTOP-DO77UPR>
2023-05-23 14:03:49 -0700

    Completed, use index2.html to access todo list

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sdi-17-todo
 A simple todo list using a javascript enabled dynamic environment
+
+Instructions on how to use.
+
+Type in whatever text in the input field and hit the "+" symbol to add that to the To do list.
+
+When you want to mark it as complete, click on the text in the todo list to strike through the item.
+
+If you want to remove the item from your list, double click it and it will be removed.
--- index2.js
+++ index2.js
@@ -1,20 +1,18 @@
-var todo = [];   // array to hold to-do list items
-const addItem = (task) => {
-    todo.push(task);
-}
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
 
-let btn = document.querySelector("#submit");
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p')
+    paragraph.classList.add('paragraph-styling')
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = '';
+    paragraph.addEventListener('click', function() {
+        paragraph.style.textDecoration = "line-through"
+    })
+    paragraph.addEventListener('dblclick', function() {
+        toDoContainer.removeChild(paragraph)
+    })
+})
 
-let myClick = () => {
-    const response = confirm('Do you want to add a new task?');
-    if (response) {
-        let input = document.getElementById("userInput").value;
-        let newTask = document.getElementsByClassName("added-task")
-        newTask.appendChild(input);
-    } else {
-        alert("Action cancelled.");
-    }
-
-}
-
-btn.addEventListener("click", myClick)
