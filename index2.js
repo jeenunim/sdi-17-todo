6a69ef49af41a4c88a252428737b122e627f8062
jeenunim <jeenunim@DESKTOP-DO77UPR>
2023-05-23 15:01:36 -0700

    Completed all of it. Still have to figure out how to save it so that the information holds even if the page is reloaded

--- index2.js
+++ index2.js
@@ -16,3 +16,18 @@ addToDoButton.addEventListener('click', function(){
     })
 })
 
+inputField.addEventListener('keypress', function(event){
+    if (event.key === "Enter") {
+    paragraph = document.createElement('p')
+    paragraph.classList.add('paragraph-styling')
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = '';
+    paragraph.addEventListener('click', function() {
+        paragraph.style.textDecoration = "line-through"
+    })
+    paragraph.addEventListener('dblclick', function() {
+        toDoContainer.removeChild(paragraph)
+    })
+    }    
+})
